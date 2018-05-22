b0fbcd84f9339ed0626b46a27198d3db4c1a7077
Nguyễn Thành Tâm <37058202+tamnt95@users.noreply.github.com>
2018-05-22 14:43:59 +0700

    Add files via upload

---
+++ .gitignore/main.js
@@ -0,0 +1,83 @@
+const socket = io('https://mypeer3000.herokuapp.com/');
+
+$('#div-chat').hide();
+
+socket.on('DANH_SACH_ONLINE', arrUserInfo => {
+	$('#div-chat').show();
+	$('#div-dangky').hide();
+
+	arrUserInfo.forEach(user => {
+		// console.log(arrUserInfo);
+		const { ten, peerID } = user;
+		$('#uluser').append(`<li id="${peerID}">${ten}</li>`); // O day su dung dau ` o duoi nut ESC
+	});
+	socket.on('CO_NGUOI_DUNG_MOI', user => {
+	// console.log(user);
+	const{ten, peerID} = user;
+		$('#uluser').append(`<li id="${peerID}">${ten}</li>`);// O day su dung dau ` o duoi nut ESC
+	});
+	socket.on('AI_DO_DA_NGAT_KET_NOI', peerID => {
+		$(`#${peerID}`).remove();
+	})
+});
+
+socket.on('DANG_KY_THAT_BAI',() => alert('Vui long chon username khac!'));
+
+
+
+function openStream(){
+	const config = {audio: false, video: true};
+	return navigator.mediaDevices.getUserMedia(config);
+}
+
+function playStream(idVideoTag, stream){
+	const video = document.getElementById(idVideoTag);
+	video.srcObject = stream; // buoc nay neo cai stream vao srcObject cung video
+	video.play();
+}
+
+openStream()
+.then(stream => playStream('localStream',stream));
+
+// var clientid = Math.random().toString(36).slice(2).slice(0, 6);
+var peer = new Peer({key: 'peerjs', host: 'frtwebrtc.herokuapp.com', port: 443, secure: true,});
+// const peer = new Peer({ key: 'tkv5g2acaree9udi'});
+peer.on('open', id => {
+	$('#my-peer').append(id);
+	$('#btnSignUp').click(() => {
+		const username = $('#txtUsername').val();
+		socket.emit('NGUOI_DUNG_DANG_KY',{ ten: username, peerID: id });
+	});
+});
+
+//caller
+$('#btnCall').click(()=> {
+	const id = $('#remoteId').val();
+	openStream()
+	.then(stream => {
+		playStream('localStream', stream);
+		const call = peer.call(id, stream);
+		call.on('stream', remoteStream => playStream('remoteStream', remoteStream));
+	});
+});
+
+//Callee
+peer.on('call', call => {
+	openStream()
+	.then(stream => {
+		call.answer(stream);
+		playStream('localStream', stream);
+		call.on('stream', remoteStream => playStream('remoteStream', remoteStream));
+	});
+});
+
+$('#uluser').on('click', 'li', function(){
+	const id = $(this).attr('id');
+	openStream()
+	.then(stream => {
+		playStream('localStream', stream);
+		const call = peer.call(id, stream);
+		call.on('stream', remoteStream => playStream('remoteStream', remoteStream));
+	});
+});
+
